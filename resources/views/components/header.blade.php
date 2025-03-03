ee3edd7d9c3e211c9f562d2b454ea639ebed555e
gygabi1996 <gyorossygabi@gmail.com>
2025-03-03 17:17:43 +0100

    implement CRUD functionality with UI design

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <!-- Header -->
-<header class="bg-blue-900 text-white p-4">
+<header class="bg-blue-900 text-white p-4" x-data="{open: false}">
     <div class="container mx-auto flex justify-between items-center">
         <h1 class="text-3xl font-semib old">
             <a href="{{url('/')}}">Workopia</a>
@@ -15,6 +15,7 @@
             <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link>
         </nav>
         <button
+            @click="open = !open"
             id="hamburger"
             class="text-white md:hidden flex items-center"
         >
@@ -22,7 +23,7 @@
         </button>
     </div>
     <!-- Mobile Menu -->
-    <nav id="mobile-menu" class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
+    <nav x-show="open" @click.away="open = false" id="mobile-menu" class="md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
         <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
         <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
         <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">Login</x-nav-link>
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,12 +1,9 @@
 <x-layout>
-    <h1>Jobs available</h1>
-    <ul>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse($jobs as $job)
-            <li>
-                <a href="{{route('jobs.show', $job->id)}}">{{ $job->title }} - {{ $job->description }}</a>
-            </li>
+            <x-job-card :job="$job" />
         @empty
             <li>No jobs available</li>
         @endforelse
-    </ul>
+    </div>
 </x-layout>
